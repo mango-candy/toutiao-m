901e4897c3fe379aa455b3a66c296a1f7364b451
mango-candy <307688583@qq.com>
2020-12-14 22:54:22 +0800

    列表封面布局开始

---
+++ src/components/article-item/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="article-item"></div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style scoped lang="less"></style>
