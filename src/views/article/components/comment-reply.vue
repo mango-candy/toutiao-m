50114ccdd52a5aa4f4cc67b7e7f5c5fb864d6eab
mango-candy <307688583@qq.com>
2020-12-22 21:38:15 +0800

    喜大普奔!喜大普奔!,文章板块终于全部结束,可喜可贺可喜可贺

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -13,27 +13,36 @@
     <!-- 评论回复列表 -->
     <van-cell title="全部回复" />
     <comment-list
+    :list="commentList"
     :source="comment.com_id"
     type="c"
     />
     <!-- 评论回复列表 -->
     <!-- 发布评论 -->
     <div class="post-wrap">
-        <van-button class="post-btn" size="small" round>写评论</van-button>
+        <van-button class="post-btn" size="small" round @click="isPostShow=true">写评论</van-button>
     </div>
-    <!-- 发布评论 -->
     </div>
+     <van-popup v-model="isPostShow" position="bottom">
+      <comment-post
+      :target="comment.com_id"
+      @post-success="onPostSuccess"
+      />
+      </van-popup>
+      <!-- 发布评论 -->
   </div>
 </template>
 
 <script>
 import CommentItem from './comment-item'
 import CommentList from './comment-list'
+import CommentPost from './comment-post'
 export default {
   name: 'CommentReply',
   components: {
     CommentItem,
-    CommentList
+    CommentList,
+    CommentPost
   },
   props: {
     comment: {
@@ -42,13 +51,25 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      isPostShow: false,
+      commentList: [] // 评论的回复列表
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onPostSuccess (data) {
+      // 更新回复的数量
+      this.comment.reply_count++
+      // 关闭弹层
+      this.isPostShow = false
+      // 将最新回复的数据展示到列表的顶部
+      this.commentList.unshift(data.new_obj)
+    }
+  }
 }
 </script>
 <style scoped lang="less">
